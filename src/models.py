34345e01735b13fccc781217f3df8ff373a14fdd
PauFugit <paulinefugit@gmail.com>
2022-09-09 04:29:59 +0000

    base

--- src/models.py
+++ src/models.py
@@ -8,23 +8,51 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250))
+    password = Column(String(250))
+    inscription_date = Column(String(250))
+    name = Column(String(250))
+    lastname = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    height = Column(String(250))
+    mass = Column(String(250), nullable=False)
+    eye_color = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(250))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    diameter = Column
+    climate = Column
+    surface_water = Column
+    population = Column
+
+class Film(Base):
+    __tablename__ = 'film'
+    id = Column(Integer, primary_key=True)
+    title = Column
+    director = Column
+    producer = Column
+    release_date = Column
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column
+    model = Column
+    manufacturer = Column
+    cost_in_credits = Column
+    crew = Column
+    passengers = Column 
 
     def to_dict(self):
         return {}
